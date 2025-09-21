b94f08752694a14071c347a260c6b070b2b423c8
Neetesh <neeteshking21@gmail.com>
2025-09-21 20:48:36 +0530

    FEAT:: UI stucture for AI prompt.
    Signed-off-by: Neetesh <neeteshking21@gmail.com>

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from ai.disease.diseaseModel import DiseaseModel
 # Load once (fast, no retraining)
 disease_model = DiseaseModel()
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def index():
+    print("here")
+    return render_template("home/index.html")
+
 @app.route("/predict", methods=["GET"])
-def predict():
+def predict_get():
     # Get query parameters
     symptoms = request.args.get("symptoms", "")
     min_score = float(request.args.get("min_score", 0.1))
     
     results = disease_model.find_disease_by_symptoms(symptoms, min_score=min_score)
     return jsonify(results)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Get query parameters
+    request_data = request.json()
+    symptoms = request_data.get("symptoms", "")
+    min_score = float(request_data.get("min_score", 0.1))
+    
+    results = disease_model.find_disease_by_symptoms(symptoms, min_score=min_score)
+    return jsonify(results)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
